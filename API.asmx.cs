51de72464da4830edf863d4928f4e3dc5314c660
Infinite Loop Development Ltd <info@infiniteloop.ie>
2024-01-12 19:12:03 +0100

    updating QR url

--- API.asmx.cs
+++ API.asmx.cs
@@ -18,12 +18,25 @@ namespace AuthenticatorAPI
         {
             var tfa = new TwoFactorAuthenticator();
             var setupInfo = tfa.GenerateSetupCode(appName, appInfo, secretCode, 300, 300);
+
+            var newQrUrl = ConvertToNewUrl(setupInfo.QrCodeSetupImageUrl);
+               
             var html = "<a title='Manually pair with " + setupInfo.ManualEntryKey +
-                       "' href='https://www.authenticatorapi.com'><img src='" + setupInfo.QrCodeSetupImageUrl.Replace("http://","https://") +
+                       "' href='https://www.authenticatorapi.com'><img src='" + newQrUrl.Replace("http://","https://") +
                        "' border=0></a>";
             return new PairingInfo { ManualSetupCode = setupInfo.ManualEntryKey, Html = html };
         }
 
+
+        static string ConvertToNewUrl(string originalUrl)
+        {
+            // Replace the necessary parts of the original URL
+            var convertedUrl = originalUrl.Replace("https://chart.googleapis.com/chart?cht=qr&chs=", "https://api.qrserver.com/v1/create-qr-code/?size=");
+            convertedUrl = convertedUrl.Replace("&chl=", "&data=");
+
+            return convertedUrl;
+        }
+        
         [WebMethod]
         public bool ValidatePin(string pin, string secretCode)
         {
